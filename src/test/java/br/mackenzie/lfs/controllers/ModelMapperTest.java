00ce51d44540248a5dcfcf73bea8f9c0b773dd68
DanielDDDL <daniel.dddl.pdf@gmail.com>
2018-02-02 15:07:04 -0200

    and this is what we are going to work with!

--- src/test/java/br/mackenzie/lfs/controllers/ModelMapperTest.java
+++ src/test/java/br/mackenzie/lfs/controllers/ModelMapperTest.java
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.ui.ModelMap;
 
 import java.time.LocalDateTime;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.Assert.assertEquals;
 
@@ -51,6 +52,44 @@ public class ModelMapperTest {
 
         assertEquals(testingCopy,book);
 
+    }
+
+    @Test
+    public void testingAsIfWasTheRealThing() throws InterruptedException {
+
+        LocalDateTime initialValue = LocalDateTime.now();
+
+        // let's assume these values are coming from a database, alright?
+        Book fromDatabase = new Book();
+        fromDatabase.setId(1L);
+        fromDatabase.setTitle("First title");
+        fromDatabase.setAuthor("First author");
+        fromDatabase.setDateWhenRegistered(initialValue);
+        fromDatabase.setDateWhenLastEdited(initialValue);
+
+        // and this one is the request object coming from the view
+        UpdateBookDTO updateBook = new UpdateBookDTO();
+        updateBook.setId(1L);
+        updateBook.setTitle("New title");
+        updateBook.setAuthor("New author");
+        updateBook.setDateWhenLastEdited(initialValue.plusHours(2L));
+
+        ModelMapper mapper = new ModelMapper();
+        mapper.map(updateBook,fromDatabase);
+
+        // so what i'm assuming will happen:
+        // the fallowing fields will have their value updated:
+        // - title, author and dataWhenLastEdited
+        // but the register date will be kept the same
+        // hence the fallowing tests
+
+        assertEquals(fromDatabase.getId(), new Long(1));
+        assertEquals(fromDatabase.getTitle(), updateBook.getTitle());
+        assertEquals(fromDatabase.getAuthor(), updateBook.getAuthor());
+        assertEquals(fromDatabase.getDateWhenRegistered(), initialValue);
+        assertEquals(fromDatabase.getDateWhenLastEdited(), updateBook.getDateWhenLastEdited());
+
+        log.info("Book results: " + fromDatabase.toString());
     }
 
 }
